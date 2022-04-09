73be9c0c2dc850604f5c336e0bd17f23f2691048
amer1can <amer1can@mail.ru>
2022-04-09 10:24:30 +0300

    authenticate is done

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,7 @@
-import {Controller, Post, UseGuards, Request} from '@nestjs/common';
+import {Controller, Post, UseGuards, Request, Get} from '@nestjs/common';
 import {LocalAuthGuard} from "./auth/local-auth.guard";
 import {AuthService} from "./auth/auth.service";
+import {JwtAuthGuard} from "./auth/jwt-auth.guard";
 
 @Controller()
 export class AppController {
@@ -10,6 +11,12 @@ export class AppController {
     @Post('auth/login')
     async login(@Request() req) {
         return this.authService.login(req.user)
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user
     }
 
 }
